4982cfdb96895770f87a576f4d2d7334f60af75b
Matthias Kollmann <matthias@kollmann-bruchsal.de>
2021-05-19 08:55:47 +0200

    added audio endpoint

--- rtsp.py
+++ rtsp.py
@@ -12,27 +12,41 @@ from gi.repository import Gst, GstRtspServer, GLib
 loop = GLib.MainLoop()
 Gst.init(None)
 
-class MyFactory(GstRtspServer.RTSPMediaFactory):
-        def __init__(self):
-                GstRtspServer.RTSPMediaFactory.__init__(self)
+class VideoFactory(GstRtspServer.RTSPMediaFactory):
+    def __init__(self):
+        GstRtspServer.RTSPMediaFactory.__init__(self)
 
-        def do_create_element(self, url):
-                pipeline_str = "( v4l2src ! video/x-h264,width=1280,height=720,framerate=30/1,profile=high ! rtph264pay name=pay0 pt=96 )" # Use camera h264
-                if len(sys.argv) > 1:
-                        pipeline_str = " ".join(sys.argv[1:])
-                print(pipeline_str)
-                return Gst.parse_launch(pipeline_str)
+    def do_create_element(self, url):
+        pipeline_str = "( v4l2src ! video/x-h264,width=1280,height=720,framerate=30/1,profile=high ! rtph264pay name=pay0 pt=96 )" # Use camera h264
+        return Gst.parse_launch(pipeline_str)
+
+
+class AudioFactory(GstRtspServer.RTSPMediaFactory):
+    def __init__(self):
+        GstRtspServer.RTSPMediaFactory.__init__(self)
+
+    def do_create_element(self, url):
+        print("Creating element for:" , url)
+        pipeline_str = "( alsasrc ! queue ! mulawenc ! rtppcmupay name=pay0 )"
+        return Gst.parse_launch(pipeline_str)
+
 
 class GstServer():
-        def __init__(self):
-                self.server = GstRtspServer.RTSPServer()
-                f = MyFactory()
-                f.set_shared(True)
-                m = self.server.get_mount_points()
-                m.add_factory("/test", f)
-                self.server.attach(None)
+    def __init__(self):
+        self.server = GstRtspServer.RTSPServer()
+
+        vid_fact = VideoFactory()
+        vid_fact.set_shared(True)
+
+        aud_fact = AudioFactory()
+        aud_fact.set_shared(True)
+
+        m = self.server.get_mount_points()
+        m.add_factory("/video", vid_fact)
+        m.add_factory("/audio", aud_fact)
+        self.server.attach(None)
 
 if __name__ == '__main__':
-        s = GstServer()
-        loop.run()
+    s = GstServer()
+    loop.run()
 
